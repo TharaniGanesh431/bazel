853fab9b827f1bb1f43ed6e06f6c458d7ec55487
Tharani <82664004+TharaniGanesh431@users.noreply.github.com>
2023-10-10 21:49:46 +0530

    Add files via upload

---
+++ bazel-springBoot/api/src/test/java/com/example/bazelspringBoot/MainTest.java
@@ -0,0 +1,11 @@
+package com.example.bazelspringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTest {
+    @Test
+    void contextLoads() {
+    }
+}
